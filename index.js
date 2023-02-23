008702cc237c1a0780dc2c8559c53206f5b73db4
Sathvik <101496703+sathvikcodes@users.noreply.github.com>
2023-02-23 18:44:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomenumber_var = Math.floor(Math.random()*6) + 1;
 var target = document.querySelectorAll("img")[0];
-target.setAttribute("src","images/dice"+(randomenumber_var)+'.png');
+target.setAttribute("src","dice"+(randomenumber_var)+'.png');
 var randomenumber_var2 = Math.floor(Math.random()*6) + 1;
 var target1 = document.querySelectorAll("img")[1];
-target1.setAttribute("src","images/dice"+(randomenumber_var2)+'.png');
+target1.setAttribute("src","dice"+(randomenumber_var2)+'.png');
 if(randomenumber_var > randomenumber_var2)
 {
     document.querySelector("h1").textContent = "Player1 has won!!!";
